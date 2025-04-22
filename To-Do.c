6ef961cacc4acfa2151e76d2e2b6fe6567a84ec8
Sheepher <ovejeromarcosluciano@gmial.com>
2025-04-22 13:51:15 -0300

    agregue la funcion mostrar tareas

--- To-Do.c
+++ To-Do.c
@@ -20,7 +20,7 @@ typedef struct nodoT
 nodoT * CreaLilstaVacia();
 nodoT * CrearNuevaTarea(char * descripcion, int duracion, int * Pid);
 void insertarNodo(nodoT **start, nodoT * nodoTnum);
-
+void mostrarTareas(nodoT * start);
 int main () {
 
     int duracion, id = 1000, *pid;
@@ -28,7 +28,7 @@ int main () {
     char descripcion[TAMA];
     printf("Creando tareas pendientes\n");
     printf("Ingrese una descripcion de la tarea peniente: ");
-    gets(descripcion);
+    fgets(descripcion, sizeof(descripcion), stdin);
     printf("Ingrese la duracion de la tarea en horas (entre 10 y  100): ");
     scanf("%d", &duracion);
 
@@ -36,17 +36,9 @@ int main () {
     
     nodoT * nodoT1 = CrearNuevaTarea(descripcion, duracion, pid);
     insertarNodo(&Pendientestart, nodoT1);
+    mostrarTareas(Pendientestart);
 
-    nodoT * aux = Pendientestart;
-
-    while (aux)
-    {
-        printf("Duracion: %d", aux->tarea.Duracion);
-        printf("\nDescripcion: %s", aux->tarea.Descripcion);
-        printf("\nId: %d", aux->tarea.TareaID);
-        
-        aux = aux->siguiente;
-    }
+    
     
 }
 nodoT * CreaLilstaVacia() {
@@ -65,4 +57,16 @@ void insertarNodo(nodoT **start, nodoT * nodoTnum) {
 
     nodoTnum->siguiente = *start;
     *start = nodoTnum;
+}
+void mostrarTareas (nodoT *start) {
+    nodoT * aux = start;
+
+    while (aux)
+    {
+        printf("Duracion: %d", aux->tarea.Duracion);
+        printf("\nDescripcion: %s", aux->tarea.Descripcion);
+        printf("\nId: %d", aux->tarea.TareaID);
+        
+        aux = aux->siguiente;
+    }
 }
